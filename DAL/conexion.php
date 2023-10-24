6cb5e452ea8e4eff46bd654dabeb68c64b55b909
CisnerosJAVA <cabeto19851@gmail.com>
2023-10-23 22:11:45 -0600

    Primer commit, script de tablas, conexion a db

---
+++ DAL/conexion.php
@@ -0,0 +1,23 @@
+<?php
+
+
+function Conecta() {
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $dataBase = "SweetSeasons";
+
+    //1. Establecer la conexion mysqli
+    $conexion = mysqli_connect($server, $user, $password, $dataBase);
+
+    if(!$conexion){
+        echo "Ocurrió un error al establecer la conexión" . mysqli_connect_error();
+    }
+
+    return $conexion;
+}
+
+function Desconecta($conexion) {
+    mysqli_close($conexion);
+}
+?>
