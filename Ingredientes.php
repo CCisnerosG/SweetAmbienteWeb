cc83f42dc9477109c8dba811a085cadca4b52d12
FerUwu19 <114791752+FerUwu19@users.noreply.github.com>
2023-11-23 14:42:16 -0600

    Estilos a ingredientes y opcion de navbar

--- Ingredientes.php
+++ Ingredientes.php
@@ -3,14 +3,22 @@ include "include/templates/header.php";
 ?>
     <br>
         <h3 class="h3">Ingredientes </h3>
-        <main>
-            <section class="container">
-                <?php
-                echo "<button class='btn' type='submit' name='Insertar'><a href='IngredientesInsert.html'>Agregar</a></button>";
-                 
-                include_once('IngredientesRead.php'); 
-                ?>
-            </section>
+        <button class='btn' type='submit' name='Insertar'><a href='IngredientesInsert.html'>Agregar</a></button>
+
+        <form method ="post" action="IngredientesModificar.html">    
+            <button class="btn" type="submit" name="ingredientesModificar">Modificar</button>
+        </form>
+
+        <main role="main">
+            <div class="album py-5 ">
+                <div class="container">
+                    <div class="row">
+                        <?php
+                        include_once('IngredientesRead.php'); 
+                        ?>
+                    </div>
+                </div>
+            </div>
         </main>
         
 
--- include/templates/header.php
+++ include/templates/header.php
@@ -14,14 +14,13 @@
         crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Mountains+of+Christmas:wght@400;700&display=swap" rel="stylesheet">
 
-    <link rel="preload" href="style/styles.css">
-    <link rel="stylesheet" href="style/styles.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.2/css/all.css" crossorigin="anonymous">
 
-    <link rel="preload" href="style/normalize.css">
+    <link rel="stylesheet" href="style/styles.css">
     <link rel="stylesheet" href="style/normalize.css">
 </head>
 <body>
-    <header class="head">
+    <!-- <header class="head">
     <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container-fluid">
                 <a class="MenuText" href="index.html">Inicio</a>
@@ -32,27 +31,27 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="display: flex; justify-content: space-between;">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="Productos.php">Productos</a>
+                            <a class="nav-link active" aria-current="page" style="font-size: 1rem; font-weight: 500; font-family:'Inter', sans-serif; color: #4F260A;" href="Productos.php">Productos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="MenuLink" href="Categorias.php">Categoría de productos</a>
+                            <a class="nav-link" style="font-size: 1rem; font-weight: 500; font-family:'Inter', sans-serif; color: #4F260A;" href="Categorias.php">Categoría de productos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="MenuLink" href="Ventas.php">Ventas</a>
+                            <a class="nav-link" style="font-size: 1rem; font-weight: 500; font-family:'Inter', sans-serif; color: #4F260A;" href="Ventas.php">Ventas</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="Compras.php">Compras</a>
+                            <a class="nav-link" style="font-size: 1rem; font-weight: 500; font-family:'Inter', sans-serif; color: #4F260A;" href="Compras.php">Compras</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="Ingredientes.php">Ingredientes</a>
+                            <a class="nav-link" style="font-size: 1rem; font-weight: 500; font-family:'Inter', sans-serif; color: #4F260A;" href="Ingredientes.php">Ingredientes</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="Proveedores.php">Proveedores</a>
+                            <a class="nav-link" style="font-size: 1rem; font-weight: 500; font-family:'Inter', sans-serif; color: #4F260A;" href="Proveedores.php">Proveedores</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="Clientes.php">Clientes</a>
+                            <a class="nav-link" style="font-size: 1rem; font-weight: 500; font-family:'Inter', sans-serif; color: #4F260A;" href="Clientes.php">Clientes</a>
                         </li>
                         <li class="nav-item">
                             <a id="sesionLink" class="nav-link" href="InicioSesion.html">Iniciar Sesión</a>
@@ -62,4 +61,48 @@
                 </div>
             </div>
         </nav>
-    </header>
+    </header> -->
+
+    <nav class="navbar navbar-expand-lg fixed-top navbar-scroll shadow-0" style="background-color: #F5C8DA;">
+  <div class="container">
+    <a class="navbar-brand MenuText" href="index.html">Inicio</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <!-- <i class="fa-solid fa-bars"></i> -->
+                    <i class="fa-solid fa-cake-candles"></i>
+                </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item ">
+            <a class="nav-link " aria-current="page" style="font-family: 'Inter', sans-serif; color: #4F260A;" href="Productos.php">Productos</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" style="font-family: 'Inter', sans-serif; color: #4F260A;" href="Categorias.php">Categoría de productos</a>         
+        </li>
+        <a class="nav-link" style="font-family: 'Inter', sans-serif; color: #4F260A;" href="Ventas.php">Ventas</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" style="font-family: 'Inter', sans-serif; color: #4F260A;" href="Compras.php">Compras</a>
+        </li>
+        <li class="nav-item ">
+        <a class="nav-link" style="font-family: 'Inter', sans-serif; color: #4F260A;" href="Ingredientes.php">Ingredientes</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" style="font-family: 'Inter', sans-serif; color: #4F260A;" href="Proveedores.php">Proveedores</a>
+        </li>
+        <li class="nav-item">           
+        <a class="nav-link" style="font-family: 'Inter', sans-serif; color: #4F260A;" href="Clientes.php">Clientes</a>
+        </li>
+      </ul>
+
+      <ul class="navbar-nav flex-row">
+        <li class="nav-item">
+          <a class="nav-link pe-3" href="InicioSesion.html">
+            <i class="fa-solid fa-user" style="color: #4F260A;"></i>
+          </a>        
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
